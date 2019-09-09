92e76632a4c9719ff2ac149e2ff7fdf9db20da5f
Nicolas Caif <r.nicolascaif@gmail.com>
2019-09-08 23:32:55 -0300

    helloworld

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,23 @@
 # from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    CreateView,
+)
 from .models import Tasks
 
 
 # Create your views here.
 class TasksList(ListView):
     model = Tasks
+
+
+class TasksCreate(CreateView):
+    model = Tasks
+    fields = [
+        'name',
+        'priority',
+        'event_id',
+        'user',
+    ]
+    success_url = reverse_lazy('tasks-list')
