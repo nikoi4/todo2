8247b44fa6cdb3087e68ab3bfc37af45a97e1877
Nicolas Caif <r.nicolascaif@gmail.com>
2019-10-01 22:52:18 -0300

    implementing and testing tasks creation

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import (
+    TestCase,
+    Client,
+)
+from django.urls import reverse_lazy
 
+from tasks.models import (
+    Tasks,
+    Priorities,
+)
 # Create your tests here.
+
+TASK = {
+    'name': 'ver el partido',
+    'event_id': '1414',
+}
+
+
+class TestTasksCreate(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(
+            username='nico',
+            password='123456',
+        )
+        self.priority = Priorities.objects.create(name='High')
+        self.client = Client()
+        self.client.force_login(self.user)
+
+    def test_task_creation(self):
+        task = Tasks(**TASK)
+        task.priority = self.priority
+        task.user = self.user
+
+        task.save()
+
+        self.assertEqual(task.name, 'ver el partido')
+
+    def test_task_post(self):
+        self.client.post(
+            reverse_lazy('tasks-new'), {
+                'name': 'ver el partido',
+                'event_id': '1414',
+                'priority': '1',
+                'user': '1'
+            },
+            # follow=True
+        )
+        self.assertEqual(Tasks.objects.count(), 1)
--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.TasksList.as_view(), name='tasks-list'),
+    url(r'^create/$', views.TasksCreate.as_view(), name='tasks-new'),
 ]
